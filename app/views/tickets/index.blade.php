ee735a828399e841a0691a77237f623eceaccdd8
Salvatore Guarino <sg@deved.it>
2014-09-22 22:26:56 +0200

    fixed tickets index view on 0 tickets

--- app/views/tickets/index.blade.php
+++ app/views/tickets/index.blade.php
@@ -10,11 +10,13 @@
 <section class="content" ng-app="app" ng-controller="MainCtrl as main">
 	<div class="row">
 		<div class="col-md-12">
+      
 			<div class="box box-primary">
 				<div class="box-header">
          			<h3 class="box-title">{{trans('My tickets')}}</h3>
          		</div>
          		<div class="box-body">
+                  @if (count($tickets))
          			<table class="table table-bordered">
          				<tbody>
          					<tr>
@@ -48,8 +50,12 @@
          					@endforeach
          				</tbody>
          			</table>
+                  @else
+                  <p>{{{trans("You don't have any tickets")}}}</p>
+                  @endif
          		</div>
 			</div>
+
 		</div>
 	</div>
 </section>
