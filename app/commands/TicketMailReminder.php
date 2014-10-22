b238c5d5dfb6a38adb0bcc0847a5005dd4f3cc9a
Nicolò Zanetti <nicolo.zanetti@gmail.com>
2014-10-23 01:53:54 +0200

    Redesign for commands, bugfixes

--- app/commands/TicketMailReminder.php
+++ app/commands/TicketMailReminder.php
@@ -39,46 +39,64 @@ class TicketMailReminder extends Command {
 	 */
 	public function fire()
 	{
-		// It assignes the input value for the day range converting it to an int.
+		// Error message to check that the user input does not contain both 'report' and 'situation'.
+		if ($this->option('report') AND $this->option('situation')) {
+			echo "Impossible! \"--report|-r\" and \"--situation|-s\" are mutually exclusive.\n";
+			return null;
+		}
+
+		$message = "";
+		$now = Carbon::now();
 		$interval = intval($this->option('days'));
+
+		// This is the database query, for tickets older than $interval and still open.
+		$tickets = Models\Ticket::where('created_at', '<', $now->subDays($interval))
+								->where('open', '=', 1)
+								->get();
+
+		$ammount = count($tickets);
 
 		/* It gives feedback about:
 		*	- If it's going to send the emails.
 		*	- The ammount of days condidered specified via input or by default value. 
-		*/ 
-		if (!$this->option('lazy')) {
-			echo "I'm about to send a reminder to all open tickets more than ". $interval." ".
-			($interval == 1 ? "day" : "days").
-			" old.\n";
-		} else {echo "Searching for open tickets over ". $interval." ".
-			($interval == 1 ? "day" : "days").
-			" old.\n";
+		*/
+		if ($this->option('confirm')) {
+			if ($this->option('test') OR $this->option('situation')) {
+				$message = "[".$now."]: ";
+				$message .= "Searching for open tickets over ";
+				$message .= $interval." ".($interval == 1 ? "day" : "days")." old.\n";
+			} else {
+				$message = "[".$now."]: ";
+				$message .= "I'm about to send a reminder to all open tickets more than ";
+				$message .= $interval." ".($interval == 1 ? "day" : "days")." old.\n";
+			}
+			$message .= "- I found ".$ammount." ".($ammount == 1 ? "ticket" : "tickets");
+			$message .= ($this->option('list') ? ":" : ".")."\n";
+			echo $message;
 		}
 
-		// This is the database query, for tickets older than $interval and still open.
-		$tickets = Models\Ticket::where('created_at', '<', Carbon::now()->subDays($interval))
-								->where('open', '=', 1)
-								->get();
-
 		// It gives feedback about the status of the old and open tickets.
-		$ammount = count($tickets);
-		if ($this->option('list') OR $this->option('lazy')) {
-			echo "I found ".$ammount." ".($ammount == 1 ? "ticket" : "tickets").":\n";
+		if ($this->option('list')) {
 			foreach ($tickets as $ticket) {
-				echo "ID: ".$ticket->id.
-				", Code: ".$ticket->code.
-				", Subject: ".$ticket->subject.
-				", Assigned to: ".$ticket->assigned_to.
-				", Days passed: ".Carbon::now()->diffInDays($ticket->created_at).
-				"\n";
+				$message = "-- ID: ".$ticket->id;
+				$message .= ", Code: ".$ticket->code;
+				$message .= ", Subject: ".$ticket->subject;
+				$message .= ", Assigned to: ".$ticket->assigned_to;
+				$message .= ", Days passed: ".$now->diffInDays($ticket->created_at)."\n";
+			}
+			echo $message;
+		}
+
+		// This will send an email for each old open ticket.
+		if (!$this->option('test') AND !$this->option('situation')) {
+			foreach ($tickets as $ticket) {
+				echo "mails sent\n";
 			}
 		}
 
 		// This will send an email for each old open ticket.
-		if (!$this->option('lazy')) {
-			foreach ($tickets as $ticket) {
-
-			}
+		foreach ($this->option('report') as $address) {
+			echo $address."\n";
 		}
 
 	}
@@ -101,9 +119,12 @@ class TicketMailReminder extends Command {
 	protected function getOptions()
 	{
 		return array(
+			array('test', 't', InputOption::VALUE_NONE, 'Prevents any email from being sent.', null),
+			array('confirm', 'c', InputOption::VALUE_NONE, 'Outputs confirm of execution.', null),
+			array('list', 'l', InputOption::VALUE_NONE, 'Prints the ids of the ticket that match.', null),
 			array('days', 'd', InputOption::VALUE_OPTIONAL, 'How many days must have passed to fire a notificantion email.', 7),
-			array('list', 'l', InputOption::VALUE_NONE, 'Prints the ids of the ticket that are being mailed.', null),
-			array('lazy', 'L', InputOption::VALUE_NONE, 'Prints the infos, but doesn\'t send the mails.', null),
+			array('report', 'r', InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'It will ALSO send an email with the log to the specified email address.', null),
+			array('situation', 's', InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'It will INSTEAD send an email with the log to the specified email address.', null),
 		);
 	}
 
