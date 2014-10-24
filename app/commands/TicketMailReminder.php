416be1c65379869c24d7b35da3c6688d1c9e1048
Nicolò Zanetti <nicolo.zanetti@gmail.com>
2014-10-25 01:49:19 +0200

    Query optimization, bug fixes, stuff, cotton candy, app debugged, working and complete.

--- app/commands/TicketMailReminder.php
+++ app/commands/TicketMailReminder.php
@@ -61,6 +61,25 @@ class TicketMailReminder extends Command {
 
 		$ammount = count($tickets);
 
+		// Elaborates who is the ticket admin.
+		$admins = [];
+		function whoIsAdmin(&$admins, $ticket)
+		{
+			/* First it will check if it already queried the DB for that admin ID.
+			*  This is to reduce the number of queries to the DB.
+			*/ 
+			foreach ($admins as $admin) {
+				if ($admin->id == $ticket->assigned_to) {
+					return $admin;
+				}
+			}
+
+			// Else it will ask the DB.
+			$admin = User::where('id', '=', $ticket->assigned_to)->first();
+			array_push($admins, $admin);
+			return $admin;
+		}
+
 		/* It gives feedback about:
 		*	- If it's going to send the emails.
 		*	- The ammount of days condidered specified via input or by default value. 
@@ -87,7 +106,7 @@ class TicketMailReminder extends Command {
 				$message = "-- ID: ".$ticket->id;
 				$message .= ", Code: ".$ticket->code;
 				$message .= ", Subject: ".$ticket->subject;
-				$message .= ", Assigned to: ".$ticket->assigned_to;
+				$message .= ", Assigned to: ".whoIsAdmin($admins, $ticket)->screen_name;
 				$message .= ", Days passed: ".Carbon::now()->diffInDays($ticket->created_at)."\n";
 				echo $message;
 			}
@@ -100,29 +119,57 @@ class TicketMailReminder extends Command {
 
 		// This will send an email for each old open ticket.
 		if (!$is_situation) {
-			$admins = [];
 			foreach ($tickets as $ticket) {
-				/* First it will check if it already queried the DB for that admin ID.
-				*  This is to reduce the number of queries to the DB.
-				*/ 
-
-				// Else it will ask the DB.
-				$target = User::where('id', '=', $ticket->assigned_to)->first();
-
-				// Now it has the data to send the email.
+				// First it will discover who is the admin for this ticket.
+				$admin = whoIsAdmin($admins, $ticket);
+				exit();
+				// Now it has all the data to send the email.
 				$delay = Carbon::now()->diffInDays($ticket->created_at);
-				\Mail::send('emails.ticketMailReminder.reminder', array('ticket' => $ticket, 'target' => $target, 'delay' => $delay), function($message) use($target, $ticket) {
+				\Mail::send('emails.ticketMailReminder.reminder', array('ticket' => $ticket, 'admin' => $admin, 'delay' => $delay), function($message) use($admin, $ticket) {
 				    $message
-				    ->to($target->email, $target->screen_name)
+				    ->to($admin->email, $admin->screen_name)
 				    ->subject(sprintf(trans('It is %s days that %s is waiting for your help!'), Carbon::now()->diffInDays($ticket->created_at), $ticket->author_email));
 				});
 			}
 		}
 
-		//$recipients = $this->option('report');
+		// This will send an email to each email address given with "report" or "situation".
+		if ($recipients) {
 
-		foreach ($recipients as $recipient) {
-			echo "a".$recipient."\n";
+			// Generating the report message
+			$th = "<th style=\"padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal\">";
+			$td = "<td style=\"text-align: center;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal\">";
+			$report = '<table style="border-collapse:collapse;border-spacing:0">';
+			$report .= "<tr>";
+			$report .= $th."Ticket ID</th>";
+			$report .= $th."User</th>";
+			$report .= $th."Ticket</th>";
+			$report .= $th."Subject</th>";
+			$report .= $th."Assigned to</th>";
+			$report .= $th."Days passed</th>";
+			$report .= "</tr>";
+			foreach ($tickets as $ticket) {
+				$admin = whoIsAdmin($admins, $ticket);
+				$report .= "<tr>";
+				$report .= $td.$ticket->id."</td>";
+				$report .= $td.$ticket->author_email."</td>";
+				$report .= $td."<a href=".URL::to('tickets/code', array($ticket->code)).">".$ticket->code."</a></td>";
+				$report .= $td.$ticket->subject."</td>";
+				$report .= $td.$admin->screen_name."</td>";
+				$report .= $td.Carbon::now()->diffInDays($ticket->created_at)."</td>";
+				$report .= "</tr>";
+			}
+			$report .= "</table>";
+
+			// Send the report to each recipient
+			foreach ($recipients as $recipient) {
+				\Mail::send('emails.ticketMailReminder.report', array('report' => $report), function($message) use($recipient) {
+				    $message
+				    ->to($recipient)
+				    ->subject(sprintf(trans('[%s]: Ticket System Status Report'), Carbon::now()));
+				});
+			}
+
 		}
 
 	}
--- app/views/emails/ticketMailReminder/reminder.blade.php
+++ app/views/emails/ticketMailReminder/reminder.blade.php
@@ -4,13 +4,13 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
-		<h2>{{ sprintf(trans('Hello %s,'), $target->screen_name) }}</h2>
+		<h2>{{ sprintf(trans('Hello %s,'), $admin->screen_name) }}</h2>
 
 		<div>
 			<p>{{ sprintf(trans('There\'s a ticket which has been <span style="color: green">open</span> for %s days!'), $delay) }}</p>
 			<p>{{ trans('We need to respond promply to our customer, else they will go away!') }}</p>
 			<p>{{ trans('You can view the ticket at the following url')}}: {{ URL::to('tickets/code', array($ticket->code)); }}
-			<p>{{ sprintf(trans('The ticket has been sent from %s.'), $ticket->author_email) }}</p>
+			<p>{{ sprintf(trans('The ticket has been sent from: %s.'), $ticket->author_email) }}</p>
 			<p>{{ sprintf(trans('With suject: "%s".'), $ticket->subject) }}</p>
 			<p>{{ sprintf(trans('The full content of the message is: %s'), $ticket->description) }}</p></p>
 		</div>
